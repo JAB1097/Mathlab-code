c1b9fa5e3324325b5a0ad35d7195908ed03a0ce9
JAB1097 <50054444+JAB1097@users.noreply.github.com>
2019-04-27 11:45:37 -0500

    Add files via upload

---
+++ ejercicio 9 math.m
@@ -0,0 +1,119 @@
+clear all 
+clc
+close all 
+b=[0,0,0,0,0,0,0,0,0,0];
+disp('ingrese diez valores')
+b(1)= input(' valor');
+b(2)= input(' valor');
+b(3)= input(' valor');
+b(4)= input(' valor');
+b(5)= input(' valor');
+b(6)= input(' valor');
+b(7)= input(' valor');
+b(8)= input(' valor');
+b(9)= input(' valor');
+b(10)= input(' valor');
+
+if b(1) > b(2) && b(1) > b(3) && b(1) > b(4) && b(1) > b(5)&& b(1) > b(6)&& b(1) > b(7)&& b(1) > b(8)&& b(1) > b(9)&& b(1) > b(10)
+    disp('El numero mayor es: ')
+    disp(b(1))
+
+   
+end
+ if  b(1) < b(2) && b(1) < b(3) && b(1) < b(4) && b(1)< b(5)&& b(1) < b(6)&& b(1)< b(7)&& b(1)< b(8)&& b(1) <b(9)&& b(1)< b(10)
+    disp('El numero menor  es: ')
+    disp(b(1))
+
+end
+    
+if b(2) > b(1) && b(2) > b(3) && b(2) > b(4) && b(2) > b(5)&& b(2) > b(6)&& b(2) > b(7)&& b(2) > b(8)&& b(2) > b(9)&& b(2) > b(10)
+    disp('El numero mayor es: ')
+    disp(b(2))
+end
+ if  b(2) < b(1) && b(2) < b(3) && b(2) < b(4) && b(2)< b(5)&& b(2) < b(6)&& b(2)< b(7)&& b(2)< b(8)&& b(2) <b(9) && b(2)< b(10)
+    disp('El numero menor  es: ')
+    disp(b(2))
+
+end
+if b(3) > b(2) && b(3) > b(1) && b(3) > b(4) && b(3) > b(5)&& b(3) > b(6)&& b(3) > b(7)&& b(3) > b(8)&& b(3) > b(9)&& b(3) > b(10)
+    disp('El numero mayor es: ')
+    disp(b(3))
+end
+
+ if  b(3) < b(2) && b(3) < b(1) && b(3) < b(4) && b(3)< b(5)&& b(3) < b(6)&& b(3)< b(7)&& b(3)< b(8)&& b(3) <b(9) && b(3)< b(10)
+    disp('El numero menor  es: ')
+    disp(b(3))
+
+end
+    if b(4) > b(2) && b(4) > b(3) && b(4) > b(1) && b(4) > b(5)&& b(4) > b(6)&& b(4) > b(7)&& b(4) > b(8)&& b(4) > b(9)&& b(4) > b(10)
+    disp('El numero mayor es: ')
+    disp(b(4))
+    end
+    
+    if b(4) < b(2) && b(4) < b(3) && b(4) < b(1) && b(4)< b(5)&& b(4) < b(6)&& b(4)< b(7)&& b(4)< b(8)&& b(4) <b(9) && b(1)< b(10)
+    disp('El numero menor  es: ')
+    disp(b(4))
+
+    end
+     if b(5) < b(2) && b(5) < b(3) && b(5) < b(1) && b(5)< b(4)&& b(5) < b(6)&& b(5)< b(7)&& b(5)< b(8)&& b(5) <b(9) && b(5)< b(10)
+    disp('El numero menor  es: ')
+    disp(b(5))
+
+     end
+     if b(5) > b(2) && b(5) > b(3) && b(5) > b(1) && b(5)> b(4)&& b(5)> b(6)&& b(5)> b(7)&& b(5)> b(8)&& b(5) > b(9) && b(5)> b(10)
+    disp('El numero mayor  es: ')
+    disp(b(5))
+
+     end
+     if b(6) < b(2) && b(6) < b(3) && b(6) < b(1) && b(6)< b(5)&& b(6) < b(4)&& b(6)< b(7)&& b(6)< b(8)&& b(6) <b(9) && b(6)< b(10)
+    disp('El numero menor  es: ')
+    disp(b(6))
+
+     end
+     if b(6) > b(2) && b(6) > b(3) && b(6) > b(1) && b(6)> b(5)&& b(6) > b(4)&& b(6)> b(7)&& b(6)> b(8)&& b(6) >b(9) && b(6)> b(10)
+    disp('El numero mayor  es: ')
+    disp(b(6))
+
+    end
+     
+     if b(7) < b(2) && b(7) < b(3) && b(7) < b(1) && b(7)< b(5)&& b(7) < b(6)&& b(7)< b(4)&& b(7)< b(8)&& b(7) <b(9) && b(7)< b(10)
+    disp('El numero menor  es: ')
+    disp(b(7))
+
+     end
+    if b(7) > b(2) && b(7) > b(3) && b(7) > b(1) && b(7)> b(5)&& b(7) > b(6)&& b(7)> b(4)&& b(7)> b(8)&& b(7) >b(9) && b(7)> b(10)
+    disp('El numero mayor es: ')
+    disp(b(7))
+
+    end
+    
+ if b(8) < b(2) && b(8) < b(3) && b(8) < b(1) && b(8)< b(5)&& b(8) < b(6)&& b(8)< b(7)&& b(8)< b(4)&& b(4) <b(8) && b(8)< b(10)
+    disp('El numero menor  es: ')
+    disp(b(8))
+
+ end
+ if b(8) > b(2) && b(8) > b(3) && b(8) > b(1) && b(8)> b(5)&& b(8) > b(6)&& b(8)> b(7)&& b(8)>b(4)&& b(4) >b(8) && b(8)> b(10)
+    disp('El numero mayor  es: ')
+    disp(b(8))
+
+ end
+  if b(9) < b(2) && b(9) < b(3) && b(9) < b(1) && b(9)< b(5)&& b(9) < b(6)&& b(9)< b(7)&& b(9)< b(8)&& b(9) <b(4) && b(9)< b(10)
+    disp('El numero menor  es: ')
+    disp(b(9))
+
+  end
+    if b(9) > b(2) && b(9) > b(3) && b(9) > b(1) && b(9)> b(5)&& b(9) > b(6)&& b(9)> b(7)&& b(9)> b(8)&& b(9) > b(4) && b(9)> b(10)
+    disp('El numero mayor  es: ')
+    disp(b(9))
+
+  end
+  if b(10) < b(2) && b(10) < b(3) && b(10) < b(1) && b(10)< b(5)&& b(10) < b(6)&& b(10)< b(7)&& b(10)< b(8)&& b(10) <b(9) && b(10)< b(4)
+    disp('El numero menor  es: ')
+    disp(b(10))
+
+  end
+    if b(10) > b(2) && b(10) > b(3) && b(10) > b(1) && b(10)> b(5)&& b(10) > b(6)&& b(10)> b(7)&& b(10)> b(8)&& b(10) > b(9) && b(10)> b(4)
+    disp('El numero mayor  es: ')
+    disp(b(10))
+
+  end
